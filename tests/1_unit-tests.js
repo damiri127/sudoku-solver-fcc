82507e5cac5bdd454d042bd9b435550e97ea4d6f
damiri127 <damiricepri127@gmail.com>
2024-11-20 15:00:13 +0700

    adding functional test done

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -13,19 +13,92 @@ suite('Unit Tests', () => {
             done();
         });
         // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+        test('Logic handles a invalid characters', function (done) { 
+            let invalidPuzzle = '1x5xx2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            assert.deepEqual(solver.validate(invalidPuzzle), {error: "Invalid characters in puzzle"});
+            done();
+        });
         // Logic handles a puzzle string that is not 81 characters in length
+        test('Logic handles a puzzle string that is not 81 characters in length', function (done) { 
+            let invalidPuzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...'
+            assert.deepEqual(solver.validate(invalidPuzzle), {error: "Expected puzzle to be 81 characters long"});
+            done();
+        });
         // Logic handles a valid row placement
+        test('Logic handles a valid row placement', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            let row = 0;
+            let column = 1;
+            let value = 3;
+            assert.equal(solver.checkRowPlacement(puzzle, row, column, value), true);
+            done();
+        });
         // Logic handles an invalid row placement
+        test('Logic handles an invalid row placement', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            let row = 0;
+            let column = 1;
+            let value = 5;
+            assert.equal(solver.checkRowPlacement(puzzle, row, column, value), false);
+            done();
+        });
         // Logic handles a valid column placement
+        test('Logic handles a valid column placement', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            let row = 0;
+            let column = 1;
+            let value = 3;
+            assert.equal(solver.checkColPlacement(puzzle, row, column, value), true);
+            done();
+        });
         // Logic handles an invalid column placement
+        test('Logic handles an invalid column placement', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            let row = 0;
+            let column = 1;
+            let value = 2;
+            assert.equal(solver.checkColPlacement(puzzle, row, column, value), false);
+            done();
+        });
         // Logic handles a valid region (3x3 grid) placement
+        test('Logic handles a valid region (3x3 grid) placement', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            let row = 0;
+            let column = 1;
+            let value = 3;
+            assert.equal(solver.checkRegionPlacement(puzzle, row, column, value), true);
+            done();
+        });
         // Logic handles an invalid region (3x3 grid) placement
+        test('Logic handles a valid region (3x3 grid) placement', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+            let row = 0;
+            let column = 1;
+            let value = 6;
+            assert.equal(solver.checkRegionPlacement(puzzle, row, column, value), false);
+            done();
+        });
     });
     suite('Check deferent puzzles', function () { 
         // Valid puzzle strings pass the solver
+        test('Valid puzzle string passed the solver', function (done) { 
+            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            assert.equal(solver.solve(puzzle), '135762984946381257728459613694517832812936745357824196473298561581673429269145378');
+            done();
+        });
         // Invalid puzzle strings fail the solver
+        test('Invalid puzzle string passed the solver', function (done) { 
+            let invalidPuzzle = '1x5xx2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            assert.equal(solver.solve(invalidPuzzle), false)
+            done();
+        });
     });
     suite('Check return puzzles', function () { 
         // Solver returns the expected solution for an incomplete puzzle
+        test('Solver retruns the expected solution for an incomplete puzzle', function (done) { 
+            let incompletePuzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            assert.equal(solver.solve(incompletePuzzle), "135762984946381257728459613694517832812936745357824196473298561581673429269145378");
+            done();
+        });
     });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,214 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
+    suite('Solved a puzzle', function () {
+        let validPuzzle = {puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'};
+        let invalidPuzzle = { puzzle: '1x5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.' };
+        let incorrectPuzzle = { puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16.....' };
+        // Solve a puzzle with valid puzzle string: POST request to /api/solve
+        test('Solve a puzzle with valid puzzle string', function (done) { 
+            chai
+                .request(server)
+                .post('/api/solve')
+                .send(validPuzzle)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'solution');
+                })
+            done();
+        });
+        // Solve a puzzle with missing puzzle string: POST request to /api/solve
+        test('Solve a puzzle with missing puzzle string', function (done) { 
+            chai
+                .request(server)
+                .post('/api/solve')
+                .send({ puzzle: undefined })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Required field missing");
+                })
+            done();
+        });
+        // Solve a puzzle with invalid characters: POST request to /api/solve
+        test('Solve a puzzle with invalid characters', function (done) {
+            chai
+                .request(server)
+                .post('/api/solve')
+                .send(invalidPuzzle)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Invalid characters in puzzle");
+                })
+            done();
+        });
+        // Solve a puzzle with incorrect length: POST request to /api/solve
+        test('Solve a puzzle with incorrect length', function (done) { 
+            chai
+                .request(server)
+                .post('/api/solve')
+                .send(incorrectPuzzle)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Expected puzzle to be 81 characters long");
+                })
+            done();
+        });
+        // Solve a puzzle that cannot be solved: POST request to /api/solve
+        test('Solve a puzzle that cannot be solved', function (done) { 
+            let cannotSolvedPuzzle = {puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.'};
+            chai
+                .request(server)
+                .post('/api/solve')
+                .send(cannotSolvedPuzzle)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Puzzle cannot be solved");
+                })
+            done();
+        });
+    });
+
+    suite('Check a puzzle', function () { 
+        // Check a puzzle placement with all fields: POST request to /api/check
+        test('Check a puzzle placement with all fields', function (done) { 
+            let input = { puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.', coordinate: 'A2', value: 3 };
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(input)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.valid, true);
+                })
+            done();
+        });
+        // Check a puzzle placement with single placement conflict: POST request to /api/check
+        test('Check a puzzle placement with single placement conflict', function (done) { 
+            let input = { puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.', coordinate: 'A2', value: 8 };
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(input)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.valid, false);
+                    assert.property(res.body, 'conflict');
+                    assert.lengthOf(res.body.conflict, 1);
+                })
+            done();
+        });
+        // Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+        test('Check a puzzle placement with multiple placement conflicts', function (done) { 
+            let input = { puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.', coordinate: 'E2', value: 6 };
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(input)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.valid, false);
+                    assert.property(res.body, 'conflict');
+                    assert.lengthOf(res.body.conflict, 2);
+                })
+            done();
+        });
+        // Check a puzzle placement with all placement conflicts: POST request to /api/check
+        test('Check a puzzle placement with  all placement conflicts', function (done) { 
+            let input = { puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.', coordinate: 'A5', value: 5 };
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(input)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.valid, false);
+                    assert.property(res.body, 'conflict');
+                    assert.lengthOf(res.body.conflict, 3);
+                })
+            done();
+        });
+        // Check a puzzle placement with missing required fields: POST request to /api/check
+        test('Check a puzzle placement with missing required fields', function (done) {
+            let invalidInput = { puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.', coordinate: 'A1', value: undefined };
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(invalidInput)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Required field(s) missing");
+                })
+            done();
+        });
+        // Check a puzzle placement with invalid characters: POST request to /api/check
+        test('Check a puzzle placement with invalid characters', function (done) { 
+            let invalidPuzzle = {
+                puzzle: '1x5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914x37.',
+                coordinate: 'D3',
+                value: 4
+            };
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(invalidPuzzle)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Invalid characters in puzzle");
+                })
+            done();
+        });
+        // Check a puzzle placement with incorrect length: POST request to /api/check
+        test('Check a puzzle placement with incorrect length', function (done) { 
+            let incorrectPuzzle = {
+                puzzle: '1.5..2.8...63.12.7.2..5...1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.',
+                coordinate: 'E5',
+                value: 2
+            }
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(incorrectPuzzle)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Expected puzzle to be 81 characters long");
+                })
+            done();
+        });
+        // Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+        test('Check a puzzle placement with invalid placement coordinate', function (done) { 
+            let invalidCoordinate = {
+                puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.',
+                coordinate: 'A10',
+                value: 8
+            }
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(invalidCoordinate)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Invalid coordinate");
+                })
+            done();
+        });
+        // Check a puzzle placement with invalid placement value: POST request to /api/check
+        test('Check a puzzle placement with invalid placement value', function (done) { 
+            let invalidValue = {
+                puzzle: '1.5..2.8...63.12.7.2..5.....9..1....8.2.3674.3.7.2..9..7...8..1..16....926914.37.',
+                coordinate: 'A1',
+                value: 10
+            }
+            chai
+                .request(server)
+                .post('/api/check')
+                .send(invalidValue)
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "Invalid value");
+                })
+            done();
+        });
+    });
 
 });
 
